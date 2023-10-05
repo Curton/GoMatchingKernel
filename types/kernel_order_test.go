b4f9027b42d77a46ac9d2dfa56ea00e18babd204
Covey <liukedun@qq.com>
2023-10-05 12:05:26 +0800

    update test

--- types/kernel_order_test.go
+++ types/kernel_order_test.go
@@ -4,27 +4,16 @@ import (
 	"container/list"
 	"testing"
 	"unsafe"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestOrderSize(t *testing.T) {
-	kernelOrder := KernelOrder{
-		KernelOrderID: 0,
-		CreateTime:    0,
-		UpdateTime:    0,
-		Amount:        0,
-		Price:         0,
-		Left:          0,
-		FilledTotal:   0,
-		Status:        0,
-		Type:          0,
-		TimeInForce:   0,
-		Id:            0,
-	}
-	println(unsafe.Sizeof(kernelOrder))
+func TestKernelOrderSize(t *testing.T) {
+	assert.Equal(t, 72, int(unsafe.Sizeof(KernelOrder{})))
 }
 
 func TestList(t *testing.T) {
 	l := list.List{}
 	l.PushBack(1)
-	println(l.Len())
+	// println(l.Len())
 }
